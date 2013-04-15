f1c4717d2c305fb78a697472d756d02a722518cf
unknown <sbupathi@sbupathi-w520.apac.tibco.com>
2013-04-15 12:15:49 +0530

    Added new repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SociStream::Application.config.secret_token = 'e147db3b7f200a6706b77c263324b82ab69ff7a988056d95c2e3414c45aadadf49139a40f097f16ad56cd70aefee9fd477107bae1637367ecd3e221f71fe3630'
